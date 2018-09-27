7afb7c61fc1c5fc7ed17c9c8356709b378826174
Lucy Suddenly <suddenlylucy@gmail.com>
2018-09-27 22:38:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ Function happyHolidaysTo(name) {
 return "Happy holidays, ${name}!" }
 
 Function happyHolidayTo(holiday, name) {
-return "Happy ${holiday}, ${name}!" }
+return `Happy ${holiday}, ${name}!` }
 
 Function holidayCountdown(holiday, days) {
-return "It's ${days} days until ${holiday}!" }
+return `It's ${days} days until ${holiday}!` }
